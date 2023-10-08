93fdea024f2c748ef5eaa9f19fe5e798098fde73
RishiKesav3 <147088887+RishiKesav3@users.noreply.github.com>
2023-10-08 13:25:08 +0530

    Add New Code

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const apiBaseUrl="https://api.themoviedb.org/3";
 const apiKey="fca2d19ad75fd81b3853e159d6b4a0b6";
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w300';
 
-const moviesGrid = document.getElementById("movies-Grid")
+const moviesGrid = document.getElementById("movies-Grid");
+const searchInput=document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
+const categoryTitle=document.getElementById("category-title");
 
 async function fetchMoviesNowPlaying(){
     const response=await fetch(`${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`);
@@ -11,20 +14,30 @@ async function fetchMoviesNowPlaying(){
     displayMovies(movies);
 }
 function displayMovies(movies) {
-    moviesGrid.innerHTML = movies.map(movie =>
+    moviesGrid.innerHTML = movies.map((movie) =>
 				`<div class="movie-card">
-	            <img src="${imageBaseUrl}${movie.poster_path}"/>
-	            <p>⭐${movie.vote_average}</p>
-	            <h1>${movie.title}</h1>
-	        </div>`
+                    <img src="${imageBaseUrl}${movie.poster_path}"/>
+                    <p>⭐${movie.vote_average}</p>
+                    <h1>${movie.title}</h1>
+	            </div>`
     ).join("");
 }
+
+function  handleSearchFormSubmit(event) {
+    categoryTitle.innerHTML="Search Results";
+    event.preventDefault();
+    const searchQuery=searchInput.value;
+    const movies=searchmovies(searchQuery);
+    displayMovies(movies);
+}
+
 fetchMoviesNowPlaying();
 
 async function searchmovies(query){
     const response=await fetch(`${apiBaseUrl}/search/movie?api_keys=${apiKey}&query="${query}"`);
     const jsonResponse=await response.json();
     const movies=jsonResponse.results;
+    
     displayMovies(movies);
 }
 function displayMovies(movies){
@@ -36,7 +49,7 @@ function displayMovies(movies){
                     <p>⭐${movie.vote_average}</p>
                     <h1>${movie.title}</h1>
                 </div>
-                 `
+                `
         )
         .join("");
 }
