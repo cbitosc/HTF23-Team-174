1e2476ce45cd1dd608e6b9b2f0e6f86359adf67b
RishiKesav3 <147088887+RishiKesav3@users.noreply.github.com>
2023-10-08 08:56:00 +0530

    Add new code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiBaseUrl="https://api.themoviedb.org/3";
+const apiBaseUrl="https://api.themoviedb.org";
 const apiKey="fca2d19ad75fd81b3853e159d6b4a0b6";
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w300';
 
@@ -8,15 +8,15 @@ async function fetchMoviesNowPlaying(){
     const response=await fetch(`${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`);
     const jsonResponse=await response.json();
     const movies=jsonResponse.results;
-    console.log(movies);
+
+    displayMovies(movies)
 }
 
 function displayMovies(movies){
-    moviesGrid.innerHTML=movies
-        .map( movie =>
+    moviesGrid.innerHTML = movies.map(movie =>
         `<img src="${imageBaseUrl}${movie.poster_path}"/>
         <p>⭐${movie.vote_average}</p>
         <h1>${movie.title}</h1>`
-        ).join("");
+    ).join("");
 }
 fetchMoviesNowPlaying();
